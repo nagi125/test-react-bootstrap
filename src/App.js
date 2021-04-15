3c08fdd4d5ba93c172ce5c4185ca2a0959164daf
nagi125 <nagi125.dev@gmail.com>
2021-04-15 13:08:17 +0900

    Hooks stateサンプル追加

--- src/App.js
+++ src/App.js
@@ -5,35 +5,58 @@ import React, { Component, useState } from 'react'
 import { Button } from 'react-bootstrap'
 
 function App() {
-  const [msg] = useState("This is sample message!")
+  const [alert, setAlert] = useState("This is alert message!")
+  const [card, setCard]   = useState("This is alert message!")
 
   return (
       <React.Fragment>
-        <h1 className="bg-primary text-white display-4"></h1>
+        <h1 className="bg-primary text-white display-4">React</h1>
         <div className="container">
-          <h4 className="my-3">Hooks sample</h4>
-          <AlertMessage message={msg} />
-          <CardMessage message={msg} />
+          <h4 className="my-3">Hooks Sample</h4>
+          <AlertMessage alert={alert} setAlert={setAlert} />
+          <CardMessage card={card} setCard={setCard} />
+          <hr />
+          <div className="text-right">
+            <p>{alert}</p>
+            <p>{card}</p>
+          </div>
         </div>
       </React.Fragment>
   )
+
 }
 
 function AlertMessage(props) {
+  const data = ["Hello!", "Welcome...", "Good-Bye?"]
+
+  const actionAlert = () => {
+    const re = data[Math.floor(Math.random() * data.length)]
+    props.setAlert('message: "' + re + '".')
+  }
+
   return (
       <React.Fragment>
         <div className="alert alert-primary h5 text-primary">
-          { props.message }
+          <h5>{props.alert}</h5>
+          <button onClick={actionAlert} className="btn btn-primary">Click me!</button>
         </div>
       </React.Fragment>
   )
 }
 
 function CardMessage(props) {
+  const [count, setCount] = useState(0)
+
+  const actionCard = () => {
+    setCount(count + 1)
+    props.setCard("card counter: " + count + " count.")
+  }
+
   return (
       <React.Fragment>
-        <div className="card p-3 h5 border-primary text-center">
-          { props.message }
+        <div className="card p-3 border-dark text-center">
+          <h5>{props.card}</h5>
+          <button onClick={actionCard} className="btn btn-primary">Click me!</button>
         </div>
       </React.Fragment>
   )
