b48ee5a7a18a7bd58dac738897f1fcb21770684a
nagi125 <nagi125.dev@gmail.com>
2021-04-15 17:49:35 +0900

    use effectサンプル追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
-import React, { Component, useState } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import { Button } from 'react-bootstrap'
 
 function AlertMessage(props) {
+
+  return (
+      <React.Fragment>
+        <div className="alert alert-primary h5 text-primary">
+          <h5>{props.msg}</h5>
+        </div>
+      </React.Fragment>
+  )
+}
+
+function App() {
+  const [val, setVal]   = useState(1000)
+  const [tax1, setTax1] = useState(0)
+  const [tax2, setTax2] = useState(0)
+  const [msg, setMsg]   = useState(<p>set a price...</p>)
+
+  const doChange = (event) => {
+    setVal(event.target.value)
+  }
+
+  const doAction = () => {
+    let res = <div>
+      <p>軽減税率(8%): {tax1} 円</p>
+      <p>通常税率(10%): {tax2} 円</p>
+    </div>
+    setMsg(res)
+  }
+
+  useEffect(() => {
+    setTax1(Math.floor(val * 1.08 ))
+  })
+
+  useEffect(() => {
+    setTax2(Math.floor(val * 1.10 ))
+  })
+
+  return (
+      <React.Fragment>
+        <h1 className="bg-primary text-white display-4">React</h1>
+        <div className="container">
+          <h4 className="my-3">Hooks Sample</h4>
+          <AlertMessage msg={msg} />
+          <div className="form-group">
+            <label>Input:</label>
+            <input type="number" className="form-control" onChange={doChange} />
+          </div>
+          <button className="btn btn-primary" onClick={doAction}>Calc</button>
+        </div>
+      </React.Fragment>
+  )
+}
+
+function Sample6AlertMessage(props) {
   const data = props.data
   const msg = JSON.stringify(data)
 
@@ -34,7 +87,7 @@ function AlertMessage(props) {
   )
 }
 
-function App() {
+function Sample6App() {
   const [name, setName] = useState("")
   const [mail, setMail] = useState("")
   const [age, setAge]   = useState(0)
